010d8926f76f91d36713f4234b7e3a329a454a0f
hassanhaidera <lsphassan@gmail.com>
2022-05-15 21:07:44 +0300

    started scraping sudanbid

--- scraper.py
+++ scraper.py
@@ -1,1 +1,6 @@
 from urllib.request import urlopen
+import bs4 as bs
+sudanbid_HTML=urlopen('https://www.sudanbid.com/')
+sudanbidssoup=bs.BeautifulSoup(sudanbid_HTML,'html.parser').find_all('a','a_homelist')
+for x in sudanbidsuop:
+    print(x.get_text())
