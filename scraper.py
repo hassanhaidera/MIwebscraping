e999c148bfa83fd1c06265fb6f706870c5f0c406
hassanhaidera <lsphassan@gmail.com>
2022-05-16 11:52:20 +0200

    started work on sudanbid function

--- scraper.py
+++ scraper.py
@@ -1,6 +1,18 @@
 from urllib.request import urlopen
 import bs4 as bs
-sudanbid_HTML=urlopen('https://www.sudanbid.com/')
-sudanbidssoup=bs.BeautifulSoup(sudanbid_HTML,'html.parser').find_all('a','a_homelist')
-for x in sudanbidsuop:
-    print(x.get_text())
+import pandas as pd
+def get_Sudanbid():
+    """"
+    """
+    try :
+        sudanbid_HTML=urlopen('https://www.sudanbid.com/') #gets sudan bid HTML
+    except Exception as e:
+        return "error reaching sudanbid" 
+    sudanbid_Soup=bs.BeautifulSoup(sudanbid_HTML,'html.parser').find_all('a','a_homelist') # beautifle soup find the tender titles taged as a_homelist
+    sudanbid_list=[]#list to capture each element of the table 
+    for tendor in sudanbid_Soup:
+        sudanbid_list.append(tendor.get_text())
+    sudanbid_Df=pd.DataFrame(data=sudanbid_list,columns=["Itmes"])
+    sudanbid_list=[]
+    return sudanbid_Df
+print(get_Sudanbid())
