7f2587a4ccef0bae688f74250464a0fe78e59192
hassanhaidera <lsphassan@gmail.com>
2022-05-17 18:47:04 +0200

    added the sudan bid scraper \\to do add the link functunality

--- scraper.py
+++ scraper.py
@@ -1,6 +1,29 @@
 from urllib.request import urlopen
 import bs4 as bs
 import pandas as pd
+import re
+def Extract_Date(Date_text):
+    """Extracts date form various types of texts
+        Arguments:
+        Date_text:string
+    Returns:
+        Date
+        
+    """
+    y=Date_text.text.split(":\xa0")[1].split("\n")[0].strip() #splits texts on \ and cleans up space and end of the line
+    try: 
+        return(dateutil.parser.parse(y)) # attmepts to parse it into a date 
+    except:
+        return(y)
+         #\\to_do acctually handel the error instead of throughing it away 
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = " " 
+    
+    # return string  
+    return (str1.join(s))
+
 def get_Sudanbid():
     """"
     """
@@ -8,11 +31,38 @@ def get_Sudanbid():
         sudanbid_HTML=urlopen('https://www.sudanbid.com/') #gets sudan bid HTML
     except Exception as e:
         return "error reaching sudanbid" 
-    sudanbid_Soup=bs.BeautifulSoup(sudanbid_HTML,'html.parser').find_all('a','a_homelist') # beautifle soup find the tender titles taged as a_homelist
+    #get items
+    sudanbid_Soup=bs.BeautifulSoup(sudanbid_HTML,'html.parser')
+    items=sudanbid_Soup.find_all('a','a_homelist') # beautifle soup find the tender titles taged as a_homelist
     sudanbid_list=[]#list to capture each element of the table 
-    for tendor in sudanbid_Soup:
+    for tendor in items:
         sudanbid_list.append(tendor.get_text())
-    sudanbid_Df=pd.DataFrame(data=sudanbid_list,columns=["Itmes"])
+    sudanbid_Df=pd.DataFrame(data=sudanbid_list,columns=["Itmes"])# creat and append into the 
+    #get opening  dates
+    open_dates=sudanbid_Soup.find_all(text=re.compile("Opening Date:"))# # beautifle soup find the tender opening date using regular expresion "Opening Date:" 
     sudanbid_list=[]
+    for d in open_dates:
+        sudanbid_list.append(Extract_Date(d))   
+    sudanbid_Df["Opening Date"]=sudanbid_list
+    #get closing  dates
+    close_dates=sudanbid_Soup.find_all(text=re.compile("Deadline:")) # beautifle soup find the tender opening date using regular expresion ""Deadline:"" 
+    sudanbid_list=[]
+    for d in close_dates:
+        sudanbid_list.append(Extract_Date(d))   
+    sudanbid_Df["Closeing Date"]=sudanbid_list
+    #get company 
+    company=sudanbid_Soup.find_all("font",class_='sudanjob_orang_color') # beautifle soup find the tender posting form color // to do find a better way to do this 
+    sudanbid_list=[]
+    for i in company:
+        sudanbid_list.append(i.get_text())   
+    sudanbid_Df["company"]=sudanbid_list
+
+    links=sudanbid_Soup.find_all('a',class_="btn_class") # beautifle soup find the tender URL form buttons and Regex
+    sudanbid_list=[]
+    for i in links:
+        pass
+        #sudanbid_list.append("https://www.sudanbid.com/"+i.attrs["href"])  
+    #sudanbid_Df["URL"]=sudanbid_list
     return sudanbid_Df
-print(get_Sudanbid())
+print(get_Sudanbid().info())
+
